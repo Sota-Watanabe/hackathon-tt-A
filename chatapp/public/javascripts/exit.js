12705cd186f06bca12e3e83503e6eadc308f6576
yuriacats <yuria.soft0615@gmail.com>
2020-11-12 19:01:00 +0900

    [wip]退出部分の処理と他のユーザーへの退出処理の実装

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -3,14 +3,16 @@
 // 退室メッセージをサーバに送信する
 function exit() {
     // ユーザ名取得
-    const userName = '';
+    const userName = $('#userName').val();
+    console.log(userName);
     // 退室メッセージイベントを送信する
+    socket.emit('sendExitEvent',userName);
 
     // 退室
     location.href = '/';
 }
 
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('exitOtherEvent', function (data) {
+    $('#thread').prepend('<p>'+data + 'が退出しました</p>');
 });
