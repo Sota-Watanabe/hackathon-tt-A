d926b4ecfe8cacd440d0073509e981e73ffede14
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-11-12 20:44:48 +0900

    投稿日時を表示する機能追加

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -6,9 +6,17 @@ function publish() {
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+    const now = new Date();
+    const month = now.getMonth()+1
+    const date = now.getDate()
     // 投稿内容を送信
     const data = {
-        userName, message
+        userName: userName,
+        message: message,
+        postTime: {
+            month: month,
+            date: date,
+        }
     }
     socket.emit('sendMessageEvent', data);
     // 入力欄を初期化する
@@ -18,6 +26,7 @@ function publish() {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
-    const {userName, message} = data
-    $('#thread').prepend('<p>' + userName + 'さん : ' + message + '</p>');
+    const {userName, message, postTime} = data
+    const {month, date} = postTime
+    $('#thread').prepend('<p>' + month + '月' + date + '日' + '   '+ userName + 'さん : ' + message + '</p>');
 });
