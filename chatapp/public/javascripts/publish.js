6c9ce17d08c3e39dddfbcbf3037d82f14576e911
yuriacats <yuria.soft0615@gmail.com>
2020-11-12 21:04:59 +0900

    WIP

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -18,6 +18,21 @@ function publish() {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
+  // 受け取った変数を定義するとこ
     const {userName, message} = data
-    $('#thread').prepend('<p>' + userName + 'さん : ' + message + '</p>');
+    //$('#thread').prepend('<p>' + userName + 'さん : ' + message + '</p>');
+    if ('content' in document.createElement('template')) {
+      var tbody =document.querySelector('tbody');
+      var template = document.querySelector('#thread-template');
+
+      var clone=tamplate.content.cloneNode(ture);
+      var p=clone.querySelectorAll("p");
+      //ここから書き換え
+      p[0].textContent = userName;
+      p[1].textContent = message;
+      
+      //ここまで書き換え
+      tbody.apptendChild(clone); 
+
+    }
 });
