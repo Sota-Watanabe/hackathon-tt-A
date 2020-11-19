5f91a9fbb986de6369c60bc439cd4bc836fab0d9
kouta0530 <16jk047@ymklab.net>
2020-11-19 18:41:59 +0900

    message:返却値関係のコメントを追加

--- chatapp/public/javascripts/message.js
+++ chatapp/public/javascripts/message.js
@@ -1,15 +1,18 @@
+/* メッセージ型の配列 */
 const messages = [];// message:Message
 
 /* 
     interface Message {
-        id:int
+        id:int　タグにつける予定
         userName: String,
         message:String,
         createDate: Date;
+        追加予定
     }
 
 */
 
+/*　追加したメッセージデータを返す */
 function addMessage(data){
     const messageData = {
         id: 0,
@@ -20,25 +23,32 @@ function addMessage(data){
     }
 
     messages.push(messageData);
+    return messageData;
 }
 
+/*昇順で並び替えたメッセージ配列を返す */
 function orderbyAsec(){
     messages.sort(function(a,b){
         return a.createDate - b.createDate;
     });
+    return messages
 }
 
+/* 降順で並び替えたメッセージ配列を返す */
 function orderbyDsec(){
     messages.sort(function(a,b){
         return b.createDate - a.createDate;
     });
+    return messages;
 }
 
+/* 削除した後のメッセージ配列を変えす */
 function deleteMessage(id){
     const index = messages.findIndex(function(elm){
         return elm.id == id;
     });
     messages.splice(index);
+    return messages
 }
 
 
